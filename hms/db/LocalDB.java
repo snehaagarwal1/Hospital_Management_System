99464d9d385a111ed1abe41029fa2b0612ed8c77
snehaagarwal1 <72247696+snehaagarwal1@users.noreply.github.com>
2023-07-10 22:05:13 +0530

    update firstPage

---
+++ hms/db/LocalDB.java
@@ -0,0 +1,14 @@
+package hms.db;
+
+public class LocalDB {
+
+    private static String auth = null;
+
+    public static String getAuth() {
+        return auth;
+    }
+
+    public static boolean setAuth(String email, String Password) {
+        return false;
+    }
+}
