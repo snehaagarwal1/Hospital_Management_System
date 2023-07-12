9745a3a29c7bc6173f140b8db415a2c70ea8ed28
snehaagarwal1 <72247696+snehaagarwal1@users.noreply.github.com>
2023-07-12 17:26:12 +0530

    patientMenu update

--- hms/menus/DoctorMenu.java
+++ hms/menus/DoctorMenu.java
@@ -1,7 +1,11 @@
 package hms.menus;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
+import hms.appointments.Appointment;
+import hms.appointments.AppointmentList;
 import hms.db.LocalDB;
 import hms.util.Label;
 import hms.util.Line;
@@ -24,10 +28,32 @@ public class DoctorMenu implements Menuable {
                 choice = sc.nextInt();
                 switch (choice) {
                     case 1:
+                        ArrayList<Appointment> ap = AppointmentList.getAppointments();
+                        Label.h_label("All Booked Appointments are : ");
+                        Iterator<Appointment> itr = ap.iterator();
+                        while (itr.hasNext()) {
+                            Appointment a = itr.next();
+                            Line.horizontalLine();
+                            System.out.println("Appointment ID: " + a.getAid());
+                            System.out.print("Name : " + a.getName());
+                            System.out.print(" Age : " + a.getAge());
+                            System.out.print(" Gender : " + a.getGender());
+                            System.out.println(" Appointment Date : " + a.getA_date());
+                            System.out.println("Issue: " + a.getIssue());
+                            System.out.println("Comment : " + a.getComment());
+                            System.out.println("Status: " + a.getStatus());
+                        }
 
                         break;
 
                     case 2:
+                        AppointmentList.updateStatus();
+                        break;
+
+                    case 3:
+                        if (!AppointmentList.putMedicine()) {
+                            Label.h_label("No Appointment Found");
+                        }
 
                         break;
 
